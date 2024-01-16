9df09e8a9b73a3cbd0414973327eaac03287175d
FrankieWhite-ExeColl <105299659+FrankieWhite-ExeColl@users.noreply.github.com>
2024-01-16 15:57:44 +0000

    Error loading only partial

--- CustomersAJAX/Controllers/CustomerController.cs
+++ CustomersAJAX/Controllers/CustomerController.cs
@@ -35,12 +35,13 @@ namespace CustomersAJAX.Controllers
 
         }
 
+        [HttpPost]
         public ActionResult OnSelectCustomer(string CustomerNumber)
         {
             Tuple<List<Customer>, Customer> tuple;
             tuple = new Tuple<List<Customer>, Customer>(customers, customers[Int32.Parse(CustomerNumber)]);
 
-            return View("Customer", tuple);
+            return PartialView("_CustomerDetails", customers[Int32.Parse(CustomerNumber)]);
 
         }
 
